944a1496e6d514d4062bdc22fa4fa6d7b3b9789f
Aaron Christiansen <125aaron@gmail.com>
2018-09-11 12:34:53 -0600

    above

--- src/model/MarshmallowMash.java
+++ src/model/MarshmallowMash.java
@@ -4,7 +4,7 @@ public class MarshmallowMash
 {
 	// --------Declaration Section---------
 	private String name;
-	private double legcount;
+	private double legCount;
 	private int eyeCount;
 	private boolean hasNoses;
 	private int armCount;
@@ -15,11 +15,20 @@ public class MarshmallowMash
 		// generally boring and not as helpful
 	}
 
-	public MarshmallowMash(String name)
+	//public MarshmallowMash(String name)
+	//{
+	//	this.name = name;
+	//}
+	
+	public MarshmallowMash(String name, double legCount, int eyeCount , boolean hasNose, int arms)
 	{
 		this.name = name;
+		this.legCount = legCount; 
+		this.eyeCount = eyeCount;
+		this.hasNoses = hasNose;
+		this.armCount = arms; 
 	}
-
+	
 	public String toString()
 	{
 		String description = "";
